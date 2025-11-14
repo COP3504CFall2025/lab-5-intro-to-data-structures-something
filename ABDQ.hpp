0f3f76ca4341b36ec8537918cde1c1a1d5897b68
Your <riley.nelson.rw@gmail.com>
2025-11-13 21:58:36 -0500

    implemented ABDQ and ABQ

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -18,27 +18,161 @@ private:
 
 public:
     // Big 5
-    ABDQ();
-    explicit ABDQ(std::size_t capacity);
-    ABDQ(const ABDQ& other);
-    ABDQ(ABDQ&& other) noexcept;
-    ABDQ& operator=(const ABDQ& other);
-    ABDQ& operator=(ABDQ&& other) noexcept;
-    ~ABDQ() override;
+    ABDQ() {
+        capacity_ = 1;
+        size_ = 0;
+        front_ = 0;
+        back_ = 0;
+        data_ = new T[capacity_];
+    }
+    explicit ABDQ(std::size_t capacity) {
+        capacity_ = capacity;
+        size_ = 0;
+        front_ = 0;
+        back_ = 0;
+        data_ = new T[capacity_];
+    }
+    ABDQ(const ABDQ& other) {
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        data_ = new T[other.capacity_];
+        front_ = other.front_;
+        back_ = other.back_;
+        for (size_t i = 0; i < size_; i++) {
+            data_[i] = other.data_[i];
+        }
+    }
+    ABDQ(ABDQ&& other) noexcept {
+        capacity_ = other.capacity_;
+        size_ = other.curr_size_;
+        data_ = other.data_;
+        front_ = other.front_;
+        back_ = other.back_;
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.capacity_ = 0;
+        other.front_ = 0;
+        other.back_ = 0;
+    }
+    ABDQ& operator=(const ABDQ& other) {
+        if (this == &other) {
+            return *this;
+        }
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = other.front_;
+        back_ = other.back_;
+        delete[] data_;
+        data_ = new T[capacity_];
+        for (size_t i = 0; i < size_; i++) {
+            data_[i] = other.data_[i];
+        }
+        return *this;
+    }
+    ABDQ& operator=(ABDQ&& other) noexcept {
+        if (this == &other) {
+            return *this;
+        }
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = other.front_;
+        back_ = other.back_;
+        delete[] data_;
+        data_ = other.data_;
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.capacity_ = 0;
+        other.front_ = 0;
+        other.back_ = 0;
+        return *this;
+    }
+    ~ABDQ() override {
+        delete[] data_;
+        data_ = nullptr;
+        front_ = 0;
+        back_ = 0;
+        capacity_ = 0;
+        size_ = 0;
+    }
 
     // Insertion
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override {
+        if (size_ == capacity_) {
+            capacity_ *= SCALE_FACTOR;
+            T* temp = new T[capacity_];
+            for (size_t i = 0; i < size_; i++) {
+                temp[i] = data_[i];
+            }
+            delete[] data_;
+            data_ = temp;
+            temp = nullptr;
+        }
+        for (int i = size_-1; i >= 0; i--) {
+            data_[i+1] = data_[i];
+        }
+        data_[0] = item;
+        size_++;
+    }
+    void pushBack(const T& item) override {
+        if (size_ == capacity_) {
+            capacity_ *= SCALE_FACTOR;
+            T* temp = new T[capacity_];
+            for (size_t i = 0; i < size_; i++) {
+                temp[i] = data_[i];
+            }
+            delete[] data_;
+            data_ = temp;
+            temp = nullptr;
+        }
+        data_[size_] = item;
+        size_++;
+    }
 
     // Deletion
-    T popFront() override;
-    T popBack() override;
+    T popFront() override {
+        if (size_ == 0) {
+            throw std::runtime_error("ABS dequeue fail");
+        }
+        T returnVal = data_[0];
+        for (size_t i = 1; i < size_; i++) {
+            data_[i-1] = data_[i];
+        }
+        size_--;
+        if (size_ <= capacity_ / 4) {
+            capacity_ /= 2;
+        }
+        return returnVal;
+    }
+    T popBack() override {
+        if (size_ == 0) {
+            throw std::runtime_error("ABS pop fail");
+        }
+        T temp = data_[size_ - 1];
+        size_--;
+
+        if (size_ <= capacity_ / 4) {
+            capacity_ /= 2;
+        }
+        return temp;
+    }
 
     // Access
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override {
+        if (size_ == 0) {
+            throw std::runtime_error("ABS peek fail");
+        }
+        return data_[0];
+    }
+    const T& back() const override {
+        if (size_ == 0) {
+            throw std::runtime_error("ABS peek fail");
+        }
+        return data_[size_ - 1];
+    }
 
     // Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return size_;
+    }
 
 };
--- ABQ.hpp
+++ ABQ.hpp
@@ -7,6 +7,13 @@
 // Technically bad, but size_t isn't likely to conflict with any client code.
 using std::size_t;
 
+/*
+ * add from end
+ * remove from front
+ */
+
+
+
 template<typename T>
 class ABQ : public QueueInterface<T>{
 
@@ -17,26 +24,114 @@ class ABQ : public QueueInterface<T>{
 
 public:
     // Constructors + Big 5
-    ABQ();
-    explicit ABQ(const size_t capacity);
-    ABQ(const ABQ& other);
-    ABQ& operator=(const ABQ& rhs);
-    ABQ(ABQ&& other) noexcept;
-    ABQ& operator=(ABQ&& rhs) noexcept;
-    ~ABQ() noexcept override;
+    ABQ() {
+        capacity_ = 1;
+        curr_size_ = 0;
+        array_ = new T[capacity_];
+    }
+    explicit ABQ(const size_t capacity) {
+        capacity_ = capacity;
+        curr_size_ = 0;
+        array_ = new T[capacity_];
+    }
+    ABQ(const ABQ& other) {
+        capacity_ = other.capacity_;
+        curr_size_ = other.curr_size_;
+        array_ = new T[other.capacity_];
+        for (size_t i = 0; i < curr_size_; i++) {
+            array_[i] = other.array_[i];
+        }
+    }
+    ABQ& operator=(const ABQ& rhs) {
+        if (this == &rhs) {
+            return *this;
+        }
+        capacity_ = rhs.capacity_;
+        curr_size_ = rhs.curr_size_;
+        delete[] array_;
+        array_ = new T[capacity_];
+        for (size_t i = 0; i < curr_size_; i++) {
+            array_[i] = rhs.array_[i];
+        }
+        return *this;
+    }
+    ABQ(ABQ&& other) noexcept {
+        capacity_ = other.capacity_;
+        curr_size_ = other.curr_size_;
+        array_ = other.array_;
+        other.array_ = nullptr;
+        other.curr_size_ = 0;
+        other.capacity_ = 0;
+    }
+    ABQ& operator=(ABQ&& rhs) noexcept {
+        if (this == &rhs) {
+            return *this;
+        }
+        capacity_ = rhs.capacity_;
+        curr_size_ = rhs.curr_size_;
+        delete[] array_;
+        array_ = rhs.array_;
+        rhs.array_ = nullptr;
+        rhs.curr_size_ = 0;
+        rhs.capacity_ = 0;
+        return *this;
+    }
+    ~ABQ() noexcept override {
+        delete[] array_;
+        array_ = nullptr;
+        curr_size_ = 0;
+        capacity_ = 0;
+    }
 
     // Getters
-    [[nodiscard]] size_t getSize() const noexcept override;
-    [[nodiscard]] size_t getMaxCapacity() const noexcept;
-    [[nodiscard]] T* getData() const noexcept;
+    [[nodiscard]] size_t getSize() const noexcept override {
+        return curr_size_;
+    }
+    [[nodiscard]] size_t getMaxCapacity() const noexcept {
+        return capacity_;
+    }
+    [[nodiscard]] T* getData() const noexcept {
+        return array_;
+    }
 
     // Insertion
-    void enqueue(const T& data) override;
+    void enqueue(const T& data) override {
+        if (curr_size_ == capacity_) {
+            capacity_ *= scale_factor_;
+            T* temp = new T[capacity_];
+            for (size_t i = 0; i < curr_size_; i++) {
+                temp[i] = array_[i];
+            }
+            delete[] array_;
+            array_ = temp;
+            temp = nullptr;
+        }
+        array_[curr_size_] = data;
+        curr_size_++;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override {
+        if (curr_size_ == 0) {
+            throw std::runtime_error("ABS peek fail");
+        }
+        return array_[0];
+    }
 
     // Deletion
-    T dequeue() override;
+    T dequeue() override {
+        if (curr_size_ == 0) {
+            throw std::runtime_error("ABS dequeue fail");
+        }
+        T returnVal = array_[0];
+        for (size_t i = 1; i < curr_size_; i++) {
+            array_[i-1] = array_[i];
+        }
+        curr_size_--;
+        if (curr_size_ <= capacity_ / 4) {
+            capacity_ /= 2;
+        }
+        return returnVal;
+    }
 
 };
